ffabba67e797c32ce388bea3caa3defab3610958
Timothy <jollyp388@gmail.com>
2025-02-04 17:13:14 +0100

    Removal of auth in Apis

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -8,23 +8,23 @@ const {
 
 } = require("../controllers/orderController"); // Import controller functions
 const methodNotAllowed = require("../utilis/methodNotAllowed"); // Handle unsupported HTTP methods
-const auth = require("../middlewares/auth"); // Authentication middleware
+// const auth = require("../middlewares/auth"); // Authentication middleware
 
 const router = express.Router();
 
   // Route to get all orders
 router
-.route("/").get(auth, getAllOrders)
-.post(auth, createOrder) // Allows POST method for creating product
+.route("/").get(getAllOrders)
+.post(createOrder) // Allows POST method for creating product
 .all(methodNotAllowed); // Reject other HTTP methods
 
 
 
 router
   .route("/:id")
-  .get(auth, getOrder)
-  .patch(auth, updateOrderStatus)
-  .delete(auth, deleteOrder)
+  .get(getOrder)
+  .patch(updateOrderStatus)
+  .delete(deleteOrder)
   .all(methodNotAllowed);
 
 module.exports = router; // Export router to be used in app.js or server.js
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -8,23 +8,23 @@ const {
 
 } = require("../controllers/productController"); // Import controller functions
 const methodNotAllowed = require("../utilis/methodNotAllowed"); // Handle unsupported HTTP methods
-const auth = require("../middlewares/auth"); // Authentication middleware
+// const auth = require("../middlewares/auth"); // Authentication middleware
 
 const router = express.Router();
 
   // Route to create a product
 router
-.route("/").get(auth, allProducts)
-.post(auth, createProduct) // Allows POST method for creating product
+.route("/").get(allProducts)
+.post(createProduct) // Allows POST method for creating product
 .all(methodNotAllowed); // Reject other HTTP methods
 
 
 
 router
   .route("/:id")
-  .get(auth, singleProduct)
-  .patch(auth, updateProduct)
-  .delete(auth, deleteProduct)
+  .get(singleProduct)
+  .patch(updateProduct)
+  .delete(deleteProduct)
   .all(methodNotAllowed);
 
 module.exports = router; // Export router to be used in app.js or server.js
